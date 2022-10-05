2e1237528971cc382bd8b4f88a5556f030d40e50
María Eugenia Finozzi <mariaef-ot@ottom.com>
2022-10-05 11:46:37 -0300

    Test redux and test CheckingCards fix

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import accountsReducer from "./accountsReducer";
 
 export const store = configureStore({
--- src/tests/CheckingCards.test.js
+++ src/tests/CheckingCards.test.js
@@ -1,5 +1,8 @@
-import '@testing-library/jest-dom/extend-expect'
-import { render } from "@testing-library/react"
+import '@testing-library/jest-dom/extend-expect';
+import { render } from "@testing-library/react";
+import { getAccountsList } from "../reducers/accountsReducer"; 
+import { store } from "../reducers/store";
+import { Provider } from "react-redux";
 
 import CheckingCards from "../components/CheckingCards";
 
@@ -34,12 +37,24 @@ const accountsZero = [];
 
 describe('CheckingCardsAccount tests', () => {
     it('Render checking cards', () => {
-        const component = render(<CheckingCards accounts={accounts}/>);
+        const component = render(
+            <Provider store={store}>
+                <CheckingCards/>
+            </Provider> 
+        );
+
+        store.dispatch(getAccountsList(accounts));
         expect(component).toBeDefined();
     });
 
     it('Render zero checking cards', () => {
-        const component = render(<CheckingCards accounts={accountsZero}/>);
+        const component = render(
+            <Provider store={store}>
+                <CheckingCards/>
+            </Provider> 
+        );
+
+        store.dispatch(getAccountsList(accountsZero));
         expect(component).toBeDefined();
     });
 });
